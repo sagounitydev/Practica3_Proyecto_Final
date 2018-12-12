443d9049d43fdbddd1e843b821547a254024357b
SagoUnityDev <sago.unitydev@gmail.com>
2018-12-12 13:59:27 +0100

    Act

--- Assets/_GameAssets/Scripts/ChessItem.cs
+++ Assets/_GameAssets/Scripts/ChessItem.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class ChessItem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool abrirCofre;
+    [SerializeField] GameObject Item;
+    [SerializeField] GameObject GenItem;
+
+    private BoxCollider bc;
+    private Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+        bc = GetComponent<BoxCollider>();
+    }
+
+    void Update() {
+        anim.SetBool("AbrirCofre", abrirCofre);
+    }
+
+    private void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag == "Player") {
+            abrirCofre = true;
+            destruirItem();
+            Destroy(this.bc);
+        }
+    }
+
+    void destruirItem() {
+        if (gameObject.name == "Item") {
+            transform.position = Item.transform.position - Vector3.up * 10f;
+            Destroy(this.gameObject, 0.7f);
+        }
+    }
 }
